5d150b2733aa13675d2c261ce480b330d23611d4
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2022-09-02 08:17:53 -0300

    comentários cifrão

--- type-string.js
+++ type-string.js
@@ -12,7 +12,7 @@ const aMaiusculo = '\u0041'
 const tique = '\u2705'
 const hiragana = '\u3041'
 
-console.log(cifrao)
+//console.log(cifrao)
 console.log(aMaiusculo)
 console.log(tique)
 console.log(hiragana)
