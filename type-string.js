07c9b8346c8acd438a00132c8d9d6433d05ce7c9
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2022-09-01 22:53:12 -0300

    exemplos da Aula

---
+++ type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha =  "senhasuperseguro456";
+const StringDeNumeros = " 165465";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leandro"
+//console.log(citacao + meuNome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
