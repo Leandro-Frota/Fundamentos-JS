a92271c27d1a988a566c69c9dcecdbce7783d95d
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2022-09-06 23:49:11 -0300

    codigo conversoes e var

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dade
+// booleano
+
+// conversão implicita
+const numero = 456;
+const numeroString = "456";
+//number = transforma string em numero
+//string
+
+console.log(numero + Number(numeroString));
+
+
+// conversão explicita
---
+++ script.js
@@ -0,0 +1,6 @@
+var altura = 5;
+var comprimento = 2;
+
+area =  altura * comprimento;
+
+console.log(area);
